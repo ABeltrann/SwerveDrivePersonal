81ac4df31698e56628af5bc30d5f688f91898cfb
Alejandro Beltran <beltranalex2005@gmail.com>
2022-10-26 21:20:14 -0700

    fixed Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,15 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class  SwerveModuleConstants{
+    public static final double kWheelDiameter = Units.inchesToMeters(1.5);
+    public static final double kDriveGearRatio = 1/8.14;
+    public static final double kTurnGearRatio = 1/12.8;
+    public static final double kDriveEncoder2Meter = kWheelDiameter * Math.PI * kDriveGearRatio;
+    public static final double kTurnEncoder2Radians = kTurnGearRatio * Math.PI * 2;
+    public static final double kDriveEncoderMetersPerSecond = kDriveEncoder2Meter/60;
+    public static final double kTurnEncoder2RadiansPerSecond = kTurnEncoder2Radians/60;
+    public static final double kPTurning = .5;
+    }
+}
