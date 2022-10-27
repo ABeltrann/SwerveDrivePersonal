008f7cb93455c633f61741ed1d9a4185596535e3
Alejandro Beltran <beltranalex2005@gmail.com>
2022-10-26 20:42:02 -0700

    use for personal pc

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveModule {
+  /** Creates a new SwerveModule. */
+  TalonFX talon;
+  CANSpar 
+
+  public SwerveModule(int ) {}
+
+  
+  }
+
