a57267155bd2d2fbb9d36d82e2f8312b39c816eb
ABeltrann <beltranalex2005@gmail.com>
2022-10-27 21:00:43 -0700

    finished swerve subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,5 +35,46 @@ public final class Constants {
     //takes in the position of each module relative to the center of robot, order is Front Left, FrontRight, BackLeft, BackRight 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(new Translation2d(kDriveWidth/2, -kDriveLength/2), new Translation2d(kDriveWidth/2, kDriveLength/2), new Translation2d(-kDriveWidth/2, -kDriveLength/2), new Translation2d(kDriveWidth/2, -kDriveLength/2));
     public static final Port kGyroId = null;
+    
+    public static final int kFrontLeftTurningID = 0;
+    public static final int kFrontLeftFowardID = 0;
+    public static final int kFrontLeftFowardEncoderID = 0;
+    public static final int kFrontLeftTurningEncoderID = 0;
+    public static final boolean kFrontLeftFowardInverted = false;
+    public static final boolean kFrontLeftTurningInverted = false;
+    public static final Double kFrontLeftAbsoluteEncoderOffset = null;
+    public static final boolean kFrontLeftAbsoluteEncoderInverted = false;
+    public static final int kFrontLeftAbsoluteEncoderID = 0;
+    
+    public static final int kFrontRightTurningID = 0;
+    public static final int kFrontRightFowardID = 0;
+    public static final int kFrontRightFowardEncoderID = 0;
+    public static final int kFrontRightTurningEncoderID = 0;
+    public static final boolean kFrontRightFowardInverted = true;
+    public static final boolean kFrontRightTurningInverted = false;
+    public static final Double kFrontRightAbsoluteEncoderOffset = 13.5;
+    public static final boolean kFrontRightAbsoluteEncoderInverted = false;
+    public static final int kFrontRightAbsoluteEncoderID = 0;
+    
+    public static final int kBackLeftTurningID = 0;
+    public static final int kBackLeftFowardID = 0;
+    public static final int kBackLeftFowardEncoderID = 0;
+    public static final int kBackLeftTurningEncoderID = 0;
+    public static final boolean kBackLeftAbsoluteEncoderInverted = false;
+    public static final boolean kBackLeftTurningInverted = false;
+    public static final Double kBackLeftAbsoluteEncoderOffset = null;
+    public static final boolean kBackLeftFowardInverted = false;
+    public static final int kBackLeftAbsoluteEncoderID = 0;
+   
+    public static final int kBackRightTurningID = 0;
+    public static final int kBackRightFowardID = 0;
+    public static final int kBackRightFowardEncoderID = 0;
+    public static final int kBackRightTurningEncoderID = 0;
+    public static final boolean kBackRightFowardInverted = true;
+    public static final boolean kBackRightTurningInverted = false;
+    public static final Double kBackRightAbsoluteEncoderOffset = null;
+    public static final boolean kBackRightAbsoluteEncoderInverted = false;
+    public static final int kBackRightAbsoluteEncoderID = 0;
+    public static final double kAbsoluteMaxSpeeds = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -9,31 +9,74 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.SwerveSubsystemConstants;;
+import frc.robot.Constants.SwerveSubsystemConstants;
 
 public class SwerveSubsystem extends SubsystemBase {
-  SwerveDriveOdometry odometry;
-  AHRS NavX;
-  Pose2d pose2d;
-  
+  private  SwerveDriveOdometry odometry;
+ private final AHRS NavX;
+private  Pose2d pose2d;
+ private final SwerveModule frontLeftModule = new SwerveModule(SwerveSubsystemConstants.kFrontLeftTurningID, SwerveSubsystemConstants.kFrontLeftFowardID, 
+  SwerveSubsystemConstants.kFrontLeftFowardEncoderID, SwerveSubsystemConstants.kFrontLeftTurningEncoderID, SwerveSubsystemConstants.kFrontLeftFowardInverted , 
+  SwerveSubsystemConstants.kFrontLeftTurningInverted, SwerveSubsystemConstants.kFrontLeftAbsoluteEncoderOffset, SwerveSubsystemConstants.kFrontLeftAbsoluteEncoderInverted, 
+  SwerveSubsystemConstants.kFrontLeftAbsoluteEncoderID, "Front Left Module");
+
+  private final SwerveModule frontRightModule = new SwerveModule(SwerveSubsystemConstants.kFrontRightTurningID, SwerveSubsystemConstants.kFrontRightFowardID, 
+  SwerveSubsystemConstants.kFrontRightFowardEncoderID, SwerveSubsystemConstants.kFrontRightTurningEncoderID, SwerveSubsystemConstants.kFrontRightFowardInverted , 
+  SwerveSubsystemConstants.kFrontRightTurningInverted, SwerveSubsystemConstants.kFrontRightAbsoluteEncoderOffset, SwerveSubsystemConstants.kFrontRightAbsoluteEncoderInverted, 
+  SwerveSubsystemConstants.kFrontRightAbsoluteEncoderID, "Front Right Module");
+
+  private final SwerveModule BackLeftModule = new SwerveModule(SwerveSubsystemConstants.kBackLeftTurningID, SwerveSubsystemConstants.kBackLeftFowardID, 
+  SwerveSubsystemConstants.kBackLeftFowardEncoderID, SwerveSubsystemConstants.kBackLeftTurningEncoderID, SwerveSubsystemConstants.kBackLeftFowardInverted , 
+  SwerveSubsystemConstants.kBackLeftTurningInverted, SwerveSubsystemConstants.kBackLeftAbsoluteEncoderOffset, SwerveSubsystemConstants.kBackLeftAbsoluteEncoderInverted, 
+  SwerveSubsystemConstants.kBackLeftAbsoluteEncoderID, "Back Left Module");
+
+  private final SwerveModule BackRightModule = new SwerveModule(SwerveSubsystemConstants.kBackRightTurningID, SwerveSubsystemConstants.kBackRightFowardID, 
+  SwerveSubsystemConstants.kBackRightFowardEncoderID, SwerveSubsystemConstants.kBackRightTurningEncoderID, SwerveSubsystemConstants.kBackRightFowardInverted , 
+  SwerveSubsystemConstants.kBackRightTurningInverted, SwerveSubsystemConstants.kBackRightAbsoluteEncoderOffset, SwerveSubsystemConstants.kBackRightAbsoluteEncoderInverted, 
+  SwerveSubsystemConstants.kBackRightAbsoluteEncoderID, "Front Right Module");
  
   public SwerveSubsystem() {
-  odometry = new SwerveDriveOdometry(SwerveSubsystemConstants.kDriveKinematics, NavX.getRotation2d());
+  odometry = new SwerveDriveOdometry(SwerveSubsystemConstants.kDriveKinematics, new Rotation2d(0));
   NavX  = new AHRS(Constants.SwerveSubsystemConstants.kGyroId);
   pose2d = new Pose2d();
+
+
+  
+
       
+  }
+
+  public Rotation2d getHeading(){
+   return Rotation2d.fromDegrees(NavX.getAngle());
+  }
+
+  public void resetOdometry( Pose2d pose){
+    odometry.resetPosition(pose , getHeading());
   }
 
   @Override
   public void periodic() {
+    pose2d = odometry.update(getHeading(), frontLeftModule.getState(),frontRightModule.getState(),BackLeftModule.getState(), BackRightModule.getState() );
+
+    
 
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void setSwerveModuleStates(SwerveModuleState[] states){
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, SwerveSubsystemConstants.kAbsoluteMaxSpeeds);
+    frontLeftModule.setDesiredState(states[0]);
+    frontRightModule.setDesiredState(states[1]);
+    frontRightModule.setDesiredState(states[2]);
+    frontRightModule.setDesiredState(states[3]);
+
+
   }
+
+
 }
